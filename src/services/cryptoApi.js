9fab55fa286482bbccd8d38bac081466b5d693d2
laurenamicone <laurenamicone@outlook.com>
2022-01-24 18:52:55 -0500

    remove api key

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -2,7 +2,7 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query';
 
 const cryptoApiHeaders = {
     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-    'x-rapidapi-key': '36603dab7cmsh2defdd41cbdc3ffp141b4djsn271f567fddc1'
+    'x-rapidapi-key': ''
 }
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com';
